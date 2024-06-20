c7ae84574f6b09d4bc08882cc8287c5e702bb1e9
rickyysee <156491520+rickyysee@users.noreply.github.com>
2024-06-20 00:26:21 -0700

    Initial commit
    Made a repository for the javascript foundations lesson for the Odin project.

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let user = "John", age = 25, message = "Hello";
+
+alert(message);
+
+const myBirthday = "03.13.2000";
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+console.log(color);
+
+let name = "John";
+let admin = name;
+console.log(admin);
+
+let ourPlanet = "Earth";
+let currentUserName;
+
+let z = 5 / 2;
+console.log(z);
