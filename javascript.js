2cce74fd02ebe72314e3bf5b795eba593c8547ef
rickyysee <156491520+rickyysee@users.noreply.github.com>
2024-06-21 00:19:27 -0700

    Finish the JavaScript variables and operators lesson.
    Edit the .js file to declare and operate on various variables while using "Live Preview" extension on VSCode to view code.
    
    Note: Console in Live Preview is finicky and doesn't seem to remember any declared variables. Should check if this is the case with devtools on Google Chrome as well, or just an issue with Live Preview.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,31 @@
-let user = "John", age = 25, message = "Hello";
-
-alert(message);
-
-const myBirthday = "03.13.2000";
+const MY_BIRTHDAY = "03.13.2000";
 const COLOR_RED = "#F00";
 const COLOR_GREEN = "#0F0";
 const COLOR_BLUE = "#00F";
 const COLOR_ORANGE = "#FF7F00";
 
 let color = COLOR_ORANGE;
-console.log(color);
+console.log("Do you know this color code:", color);
 
-let name = "John";
+let name = "Ricky";
 let admin = name;
-console.log(admin);
+console.log("admin is", admin);
 
 let ourPlanet = "Earth";
 let currentUserName;
 
 let z = 5 / 2;
-console.log(z);
+console.log("5/2 =", z);
 
-console.log((4 + 6 + 9)/77);
+console.log("(4 + 6 + 9) / 7 =", (4 + 6 + 9)/77);
+
+let a = 10;
+console.log(a);
+// Try this in the console: 9 * a;
+let b = 9 * a;
+console.log(b);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual/MAX;
+console.log("Percentage is", percentage);
